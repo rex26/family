c348ced9812280155b7df5e7a7054e679499bb12
rex26 <858790345@qq.com>
2020-10-20 21:58:51 +0800

    1.beanfactory post processor

---
+++ spring_learn/webDemo/src/main/java/com/linkedbear/spring/postprocessor/h_factoryprocessor/bean/Color.java
@@ -0,0 +1,15 @@
+package com.linkedbear.spring.postprocessor.h_factoryprocessor.bean;
+
+public abstract class Color {
+
+    protected String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
