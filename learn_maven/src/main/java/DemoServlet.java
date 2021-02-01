62baca4b29bb987b04602af8fc1da57d9a76e4b4
rex26 <858790345@qq.com>
2021-02-01 21:33:33 +0800

    1.add maven servlet demo;

---
+++ learn_maven/src/main/java/DemoServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author helios
+ */
+@WebServlet(urlPatterns = "/test")
+public class DemoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("hello beauty !");
+    }
+}
